6d4cfe87cca52fcaa79b35ae63c088fc65535a4b
quan200699 <maplestory2012vn@gmail.com>
2019-11-21 10:41:50 +0700

    ok

---
+++ src/app/photo/photo-create/photo-create.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {PhotoService} from '../../service/photo.service';
+
+@Component({
+  selector: 'app-photo-create',
+  templateUrl: './photo-create.component.html',
+  styleUrls: ['./photo-create.component.css']
+})
+export class PhotoCreateComponent implements OnInit {
+  successMessage: string;
+  failMessage: string;
+
+  constructor(private photoService: PhotoService) {
+  }
+
+  ngOnInit() {
+  }
+
+  addPhoto(photoForm) {
+    this.photoService.createPhoto(photoForm.value).subscribe(() => {
+      this.successMessage = 'Tạo mới thành công';
+    }, () => {
+      this.failMessage = 'Tạo mới thất bại';
+    });
+  }
+}
